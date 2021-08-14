fa5300f37cbf05091e19b0492cbbb6f8b79f2e84
Frederic Douville <frederic.douville@zoho.eu>
2021-08-14 22:09:46 +0200

    [https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR/issues/1]fermeture de la modal en cliquant sur x

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const closeCross = document.querySelectorAll(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeCross.forEach((btn) => btn.addEventListener("click",shutModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+function shutModal() {
+  modalbg.style.display = "none";
+}
